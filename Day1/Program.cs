0597e6fed32783bda42bedd191d7d393b60ba6b1
alex <meandu229@protonmail.com>
2022-12-01 09:02:19 +0000

    Day1 - part 1

---
+++ Day1/Program.cs
@@ -0,0 +1,15 @@
+﻿var max = 0;
+var current = 0;
+var lines = await File.ReadAllLinesAsync("input.json");
+foreach (var line in lines) 
+{
+    if(string.IsNullOrEmpty(line))
+    {
+        if (current > max)
+            max = current;
+        current = 0;
+    }
+    else
+        current = current + int.Parse(line);
+}
+Console.Write(max);
