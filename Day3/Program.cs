a750a657f5b4871ea40ab6db82f3e956ec7bc2ff
alex <meandu229@protonmail.com>
2022-12-03 09:50:12 +0000

    Day 3 P1

---
+++ Day3/Program.cs
@@ -0,0 +1,22 @@
+﻿
+using System.Diagnostics;
+using System.Linq.Expressions;
+
+var readAllLinesAsync = await File.ReadAllLinesAsync("input.txt");
+var totalPriority = 0;
+foreach(var line in readAllLinesAsync)
+{
+    var priority = 0;
+    var halfLength = line.Length/2;
+    var firstHalf = line.Substring(0, halfLength);
+    var secondHalf = line.Substring(halfLength);
+    var commonChar = firstHalf.ToList().Intersect(secondHalf.ToList());
+    if (commonChar.Count() == 0)
+        continue;;
+    if (commonChar.First() > 97)
+        priority = commonChar.First()-96;
+    else
+        priority = commonChar.First() - 38;
+    totalPriority += priority;
+}
+Console.WriteLine(totalPriority);
