e46824e61666097a3158f50dc7f018868a8fd080
alex <meandu229@protonmail.com>
2022-12-04 09:32:07 +0000

    Day 4 part 1

---
+++ Day4/Program.cs
@@ -0,0 +1,13 @@
+﻿using Day4;
+
+var readAllLinesAsync = await File.ReadAllLinesAsync("input.txt");
+var counter = 0;
+foreach (var line in readAllLinesAsync)
+{
+    var strings = line.Split(",");
+    var bitsFromRange = RangeToBits.GetBitsFromRange(strings[0], strings[1]);
+    var result = RangeToBits.CheckForContaining(bitsFromRange.rangeoneBinary, bitsFromRange.rangetwoBinary);
+    if (result)
+        counter++;
+}
+Console.WriteLine(counter);
---
+++ Tests/Day4.cs
@@ -0,0 +1,33 @@
+using Day4;
+using Xunit;
+using Assert = Xunit.Assert;
+
+namespace Tests;
+
+public class Day4
+{
+    [Theory]
+    [InlineData("2-8","3-7")]
+    public void RangeToBits_ShouldReturnCorrectBits(string range1, string range2)
+    {
+        var range1List = range1.Split("-").Select(x => int.Parse(x)).ToList();
+        var range2List = range2.Split("-").Select(x => int.Parse(x)).ToList();
+        var max = range1List.Max() > range2List.Max() ? range1List.Max() : range2List.Max();
+
+        var bitsFromRange = RangeToBits.GetBitsFromRange(range1,range2);
+        
+        Assert.Equal("01111111", bitsFromRange.rangeoneBinary);
+        
+        Assert.Equal("00111110", bitsFromRange.rangetwoBinary);
+    }
+
+    [Theory]
+    [InlineData("01111111","00111110", true)]
+    [InlineData("01111111","10101110", false)]
+
+    public void RangeToBits_CheckForContains(string binaryone, string binarytwo, bool expectedResult)
+    {
+        var checkForContaining = RangeToBits.CheckForContaining(binaryone, binarytwo);
+        Assert.Equal(checkForContaining, expectedResult);
+    }
+}
