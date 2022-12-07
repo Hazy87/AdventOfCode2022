cd30fb0edc09b949fd66fc1501520d3659f74221
alex <meandu229@protonmail.com>
2022-12-07 09:46:27 +0000

    Day 7 part 1

---
+++ Day7/Program.cs
@@ -0,0 +1,56 @@
+﻿using System.Globalization;
+
+var readAllLinesAsync = (await File.ReadAllLinesAsync("input.txt")).ToList();
+var directories = new List<Directory>();
+Directory currentDirectory = null;
+foreach (var line in readAllLinesAsync)
+{
+    if (line == "$ cd ..")
+    {
+        currentDirectory = currentDirectory.Parent;
+    }
+    else if (line.StartsWith($"$ cd"))
+    {
+        currentDirectory = new Directory(){Name = line.Split(" ")[2], Parent = currentDirectory};
+        directories.Add(currentDirectory);
+    }
+
+    else if (line.StartsWith("dir"))
+    {
+        
+    }
+    else if (Helper.IsFile(line))
+    {
+        currentDirectory.fileSizes.Add(int.Parse(line.Split(" ")[0]));
+    }
+}
+int GetSize(Directory directory1, List<Directory> list)
+{
+    var size = 0;
+    foreach (var child in list.Where(x => x.Parent == directory1))
+    {
+        size += GetSize(child, list);
+    }
+
+    size += directory1.fileSizes.Sum();
+    return size;
+}
+
+var sum = directories.Where(x => GetSize(x, directories) <= 100000).Sum(x => GetSize(x, directories));
+Console.WriteLine(sum);
+
+public class Helper
+{
+    public static bool IsFile(string command)
+    {
+        int whocares;
+        return int.TryParse(command.Split()[0], out whocares);
+    }
+}
+class Directory
+{
+    public Directory Parent { get; set; }
+    public string Name { get; set; }
+    public List<int> fileSizes { get; set; } = new List<int>();
+    public List<string> SubDirectory { get; set; } = new List<string>();
+}
