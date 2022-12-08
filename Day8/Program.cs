c458e4bebe0d71e8171721f97d51c79afa4a15e0
alex <meandu229@protonmail.com>
2022-12-08 10:36:10 +0000

    Day 8 p2

--- Day8/Program.cs
+++ Day8/Program.cs
@@ -1,69 +1,94 @@
-﻿var treesGrid = (await File.ReadAllLinesAsync("input.txt")).ToList().Select(x => x.ToCharArray().Select(y => int.Parse(y.ToString())).ToList()).ToList();
+﻿var treesGrid = (await File.ReadAllLinesAsync("input.txt")).ToList()
+    .Select(x => x.ToCharArray().Select(y => int.Parse(y.ToString())).ToList()).ToList();
 
 
-bool LookUpwards(int treeHeight, List<List<int>> list, int x, int y)
+int LookUpwards(int treeHeight, List<List<int>> list, int x, int y)
 {
-    if (x == 0) return true;
+    var scenicScore = 1;
+    if (x == 0) return 1;
     for (int j = x; j >= 1; j--)
     {
         if (list[j - 1][y] >= treeHeight)
         {
-            return false;
+            return scenicScore;
         }
-    }
-    return true;
-}
-bool LookDownwards(int treeHeight, List<List<int>> list, int x, int y)
-{
-    if (x == 0) return true;
-    for (int j = x; j < list.Count()-1; j++)
-    {
-        if(list[j +1][y] >= treeHeight) {
-            return false;
-        }
+
+        scenicScore++;
     }
 
-    return true;
+    return scenicScore -1;
+    }
+
+int LookDownwards(int treeHeight, List<List<int>> list, int x, int y)
+{    if (x == 0) return 0;
+
+    var scenicScore = 1;
+
+    for (int j = x; j < list.Count() - 1; j++)
+    {
+        if (list[j + 1][y] >= treeHeight)
+        {
+            return scenicScore;
+        }
+
+        scenicScore++;
+    }
+
+    return scenicScore -1;
 }
-bool LookLeftwards(int treeHeight, List<List<int>> list, int x, int y)
-{
-    if (y == 0) return true;
+
+int LookLeftwards(int treeHeight, List<List<int>> list, int x, int y)
+{    if (y == 0) return 0;
+
+    var scenicScore = 1;
+
     for (int j = y; j >= 1; j--)
     {
         if (list[x][j - 1] >= treeHeight)
         {
-            return false;
+            return scenicScore;
         }
-    }
-    return true;
-}
-bool LookRightwards(int treeHeight, List<List<int>> list, int x, int y)
-{
-    if (y == 0) return true;
-    for (int j = y; j < list.Count-1; j++)
-    {
-        if(list[x][j +1] >= treeHeight) {
-            return false;
-        }
+
+        scenicScore++;
     }
 
-    return true;
+    return scenicScore -1;
 }
-var counter = 0;
-for (int x = 0; x <= treesGrid.Count-1; x++)
+
+int LookRightwards(int treeHeight, List<List<int>> list, int x, int y)
+{    if (y == 0) return 0;
+
+    var scenicScore = 1;
+
+    for (int j = y; j < list.Count - 1; j++)
+    {
+        if (list[x][j + 1] >= treeHeight)
+        {
+            return scenicScore;
+        }
+
+        scenicScore++;
+    }
+
+    return scenicScore -1;
+}
+
+var counter = new List<int>();
+for (int x = 0; x <= treesGrid.Count - 1; x++)
 {
-    
-    for (int y = 0; y <= treesGrid[x].Count-1; y++)
+    for (int y = 0; y <= treesGrid[x].Count - 1; y++)
     {
         var treeHeight = treesGrid[x][y];
-        if (LookUpwards(treeHeight, treesGrid, x, y) || LookDownwards(treeHeight, treesGrid, x, y) ||
-            LookLeftwards(treeHeight, treesGrid, x, y) || LookRightwards(treeHeight, treesGrid, x, y))
-        {
-            counter++;
-            Console.Write("T");
-        }
-        else Console.Write("F");
+        var lookUpwards = LookUpwards(treeHeight, treesGrid, x, y);
+        var lookDownwards = LookDownwards(treeHeight, treesGrid, x, y);
+        var lookLeftwards = LookLeftwards(treeHeight, treesGrid, x, y);
+        var lookRightwards = LookRightwards(treeHeight, treesGrid, x, y);
+        var scenicScore = lookUpwards * lookDownwards *
+                          lookLeftwards * lookRightwards;
+        counter.Add(scenicScore);
     }
+
     Console.WriteLine();
 }
-Console.WriteLine(counter);
+
+Console.WriteLine(counter.Max());
